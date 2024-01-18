1c47e3527839940c61b6844b637b833230b074be
Teun van den Brand <tahvdbrand@gmail.com>
2024-01-18 15:35:13 +0100

    delete scale name tests

--- tests/testthat/test-viridis.R
+++ tests/testthat/test-viridis.R
@@ -1,14 +1,8 @@
 context("scales")
 test_that("scales work correctly", {
 
-  fill_scale <- scale_fill_viridis(option = "A")
-  expect_equal(fill_scale$scale_name, "gradientn")
-
   fill_scale <- scale_fill_viridis(option = "A", discrete = TRUE)
   expect_equal(fill_scale$is_discrete(), TRUE)
-
-  color_scale <- scale_color_viridis(option = "A")
-  expect_equal(color_scale$scale_name, "gradientn")
 
   color_scale <- scale_color_viridis(option = "A", discrete = TRUE)
   expect_equal(color_scale$is_discrete(), TRUE)
